7ad6713035c1a6bebba25d345f750ac24a0bc729
Juandlobaton <juandavidlobaton@gmil.com>
2020-03-16 13:10:26 -0500

    Consultar Vehiculos por propietario

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApiEmptransporte.Models;  
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApiEmptransporte.Controllers
 {
@@ -11,19 +13,48 @@ namespace WebApiEmptransporte.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+
+        private readonly CrudTransporteContext _context;
+
+        public ValuesController(CrudTransporteContext context)
+        {
+            _context = context;
+        }
+
         // GET: api/Values
         [HttpGet]
-        public IEnumerable<string> Get()
-        {
+        public List<object> Get()
+        /*{
 
            // using (Models.CrudTransporteContext db = new Models.CrudTransporteContext())
            
 
               return new string[] { "value1", "value2" };
+        } */
+         {
+            var result = from pm in _context.Propietario join veh in _context.vehiculos on pm.PropietarioId.ToString() equals veh.Id_propietario
+                         join con in _context.conductors on veh.Id_vehiculo.ToString() equals con.Id_vehiculo
+                         select new
+                         {
+                             Vehiculo_propietario = veh.Marca,
+                             Placa_vehiculo = veh.Placa,
+                             Nombre_Propietario = pm.Nombre,
+                             Conductor_asignado = con.Nombre,
+                             Modelo_Vehiculo = veh.Modelo,
+                             Fecha_modelo = veh.Fecha
+                                 
+                         }; 
+                            
+
+                List<object> query = new List<object>(result);
+
+
+                return query;
         }
 
-        // GET: api/Values/5
-        [HttpGet("{id}", Name = "Get")]
+
+    // GET: api/Values/5
+    [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
             return "value";
@@ -46,5 +77,17 @@ namespace WebApiEmptransporte.Controllers
         public void Delete(int id)
         {
         }
+
+        public List<object> GetIn(Guid id)
+        {
+            {
+                var result = from pm in _context.vehiculos select pm;
+
+                List<object> query = new List<object>(result);
+
+
+                return query;
+            }
+        } 
     }
 }
